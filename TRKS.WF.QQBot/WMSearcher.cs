102ae1cde3ab6c9df320178b191534baa79352f8
Cyl18 <cyl18a@gmail.com>
2019-01-15 20:51:47 +0800

    解决一个傻逼的问题

--- TRKS.WF.QQBot/WMSearcher.cs
+++ TRKS.WF.QQBot/WMSearcher.cs
@@ -68,7 +68,7 @@ namespace TRKS.WF.QQBot
                             var sb = new StringBuilder();
                             var similarlist = translator.GetSimilarItem(item.Format());
                             sb.AppendLine($"物品{item}不存在或格式错误.");
-                            sb.AppendLine($"请问这下面有没有你要找的物品呢?:");
+                            sb.AppendLine($"请问这下面有没有你要找的物品呢?（可尝试复制下面的名称来进行搜索)");
                             foreach (var similarresult in similarlist)
                             {
                                 sb.AppendLine($"    {similarresult}");
