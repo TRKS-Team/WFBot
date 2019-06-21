41c0b8b3fdb6d7afe3400df44ee8fbf75617d7b1
Cyl18 <cyl18a@gmail.com>
2019-06-21 20:52:23 +0800

    fix #44

--- TRKS.WF.QQBot/MahuaEvents/AutoUpdateRR.cs
+++ TRKS.WF.QQBot/MahuaEvents/AutoUpdateRR.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using TRKS.WF.QQBot.MahuaEvents;
@@ -11,15 +12,21 @@ namespace TRKS.WF.QQBot
 {
     public static class AutoUpdateRR
     {
+        [DllImport("dnsapi.dll", EntryPoint = "DnsFlushResolverCache")]
+        private static extern UInt32 DnsFlushResolverCache();
+
+
         public static void Execute()
         {
+            DnsFlushResolverCache();
             var basePath = "YUELUO\\TRKS.WF.QQBot\\AutoUpdater.exe";
             var path = Path.GetTempFileName() + ".exe";
             File.Copy(basePath, path, true);
             File.Copy(basePath+".config", path + ".config", true);
             Process.Start(new ProcessStartInfo(path)
             {
-                WorkingDirectory = Directory.GetCurrentDirectory()
+                WorkingDirectory = Directory.GetCurrentDirectory(),
+                UseShellExecute = false
             });
 
             Trace.WriteLine("Started AutoUpdate.", "AutoUpdate");
