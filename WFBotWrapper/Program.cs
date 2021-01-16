886e396b65c294883a7bf9aad9f6756faea3c302
Cyl18 <cyl18a@gmail.com>
2021-01-17 03:03:20 +0800

    test

---
+++ WFBotWrapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WFBotWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
