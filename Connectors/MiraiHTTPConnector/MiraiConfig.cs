9fb159e7bf22161bfe0215b8af0d54811aef0f65
Cyl18 <cyl18a@gmail.com>
2021-01-16 23:03:55 +0800

    dev

---
+++ Connectors/MiraiHTTPConnector/MiraiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFBot.Utils;
+
+namespace MiraiHTTPConnector
+{
+    [Configuration("MiraiConfig")]
+    public class MiraiConfig : Configuration<MiraiConfig>
+    {
+        public string Host = "127.0.0.1";
+        public short Port = 8080;
+        public string AuthKey = "";
+
+        public long BotQQ = default;
+    }
+}
