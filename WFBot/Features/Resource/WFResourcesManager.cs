19ff275097490c753636e1d871378eb8040f408b
Cyl18 <cyl18a@gmail.com>
2021-11-08 15:31:28 +0800

    另外一个命名空间也错了

--- WFBot/Features/Resource/WFResourcesManager.cs
+++ WFBot/Features/Resource/WFResourcesManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using GammaLibrary;
+using WFBot.Utils;
 
 namespace WFBot.Features.Resource
 {
