c6b00b21f510786be1309bf0587587f5f4b78922
TheRealKamisama <TheRealKamisama@163.com>
2022-01-27 20:04:56 +0800

    这就很奇怪了, 我域名过期了居然没有任何一个机器人报错?

--- WFBot/Features/Resource/WFResources.cs
+++ WFBot/Features/Resource/WFResources.cs
@@ -121,7 +121,7 @@ namespace WFBot.Features.Resource
         {
             const string source = "http://origin.warframe.com/origin/00000000/PublicExport/index_zh.txt.lzma";
             const string cdn =
-                "https://wfbot-cdn.therealkamisama.workers.dev/http://origin.warframe.com/origin/00000000/PublicExport/index_zh.txt.lzma";
+                "http://wfbot.cdn.kraber.top/http://origin.warframe.com/origin/00000000/PublicExport/index_zh.txt.lzma";
             // 似乎是Warframe服务器ban掉了阿里云的IP, 走一层cdn先
             var name = source.Split('/').Last();    
             var path = Path.Combine("WFCaches", name);
@@ -239,7 +239,7 @@ namespace WFBot.Features.Resource
                 {"Accept-Encoding", "br"}
             };
 
-            var resource = WFResource<WFCD_All[]>.Create(url: "https://wfcd-all.therealkamisama.top/", nameof(WFCD_All),
+            var resource = WFResource<WFCD_All[]>.Create(url: "https://wfcd-all.kraber.top", nameof(WFCD_All),
                 "All.json", 
                 header, 
                 ResourceLoaders<WFCD_All[]>.JsonDotNetLoader,
