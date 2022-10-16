d22fefa985bae392758bbd1a44d7fe5ae90b827b
Cyl18 <cyl18a@gmail.com>
2022-10-16 20:48:26 +0800

    Update build-wfbot-ci.bat

--- build-wfbot-ci.bat
+++ build-wfbot-ci.bat
@@ -3,6 +3,6 @@ REM dotnet publish WFBot -o out/WFBotLinuxArm64 -r linux-arm64 --self-contained 
 REM dotnet publish WFBot -o out/WFBotLinux -r linux-x64 --self-contained false -c "Linux Release" -p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 REM dotnet publish WFBot -o out/WFBotWindows -r win-x86 --self-contained false -c "Linux Release" -p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 
-dotnet publish WFBot -o out/WFBotLinuxArm64S -r linux-arm64 --self-contained true -c "Linux Release" -p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
-dotnet publish WFBot -o out/WFBotLinuxX64S -r linux-x64 --self-contained true -c "Linux Release" -p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
-dotnet publish WFBot -o out/WFBotWindowsS -r win-x86 --self-contained true -c "Linux Release" -p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish WFBot -o out/WFBotLinuxArm64S -r linux-arm64 --self-contained true -c "Linux Release" -p:PublishSingleFile=true -p:PublishReadyToRun=true /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish WFBot -o out/WFBotLinuxX64S -r linux-x64 --self-contained true -c "Linux Release" -p:PublishSingleFile=true -p:PublishReadyToRun=true /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish WFBot -o out/WFBotWindowsS -r win-x86 --self-contained true -c "Linux Release" -p:PublishSingleFile=true -p:PublishReadyToRun=true /p:IncludeNativeLibrariesForSelfExtract=true
