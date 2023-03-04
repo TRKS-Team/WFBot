a7e6952732e9b76687a76f439d3a09005542f17b
Cyl18 <cyl18a@gmail.com>
2023-03-04 20:25:38 +0800

    一定是optifine干的！

--- WFBot/Features/ImageRendering/ImageRenderHelper.cs
+++ WFBot/Features/ImageRendering/ImageRenderHelper.cs
@@ -579,8 +579,7 @@ namespace WFBot.Features.ImageRendering
                     var image = new Image<Rgba32>(50,50, new Rgba32(0,0,0));
                     image.Mutate(x => x.Fill(new Color(new Rgba32(206, 64, 202)), new RectangleF(0,0,25,25)));
                     image.Mutate(x => x.Fill(new Color(new Rgba32(206, 64, 202)), new RectangleF(25,25,25,25)));
-                    image.Mutate(x => x.DrawText(CreateTextOptions(10), "错误", Color.White));
-                    return RenderText("错误");
+                    return image;
                 }
                 Cache[path] = Image.Load<Rgba32>(stream, new PngDecoder());
             }
